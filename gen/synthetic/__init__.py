5d5b93fa7d233d2725ad1c241b03436309e89a29
Luan Moreno Medeiros Maciel <luan.moreno@owshq.com>
2026-08-22 06:40:00 -0300

    workshop: create progressive learner starter

--- gen/synthetic/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Deterministic four-entity PSP workshop generator."""
-
-from .generator import GeneratorConfig, build_frames, manifest
-
-__all__ = ["GeneratorConfig", "build_frames", "manifest"]
