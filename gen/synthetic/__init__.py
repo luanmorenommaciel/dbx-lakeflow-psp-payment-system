e9394b7cf18369ee2ab27c39e5297c4a33c96bfc
Luan Moreno Medeiros Maciel <luan.moreno@owshq.com>
2026-08-22 06:40:06 -0300

    workshop: checkpoint module 02 synthetic data

---
+++ gen/synthetic/__init__.py
@@ -0,0 +1,5 @@
+"""Deterministic four-entity PSP workshop generator."""
+
+from .generator import GeneratorConfig, build_frames, manifest
+
+__all__ = ["GeneratorConfig", "build_frames", "manifest"]
